9549dbb76895702b34e3c8e91ec9bee31fb6a5da
Jeff Johnson <jeff@classfill.com>
2012-10-10 23:04:25 -0500

    Added contact section!

--- js/main.js
+++ js/main.js
@@ -45,5 +45,9 @@
 			});
 			
 		});	    
+
+		// no spam please
+		var email_addr = [106, 111, 104, 110, 115, 111, 110, 106, 101, 102, 102, 64, 103, 109, 97, 105, 108, 46, 99, 111, 109].map(function(v) { return String.fromCharCode(v) }).join('')
+		$('#email_link').attr('title', email_addr).attr('href', 'mailto:'+email_addr)
 	});
 })();
